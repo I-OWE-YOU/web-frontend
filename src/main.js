78e6498dcfe4a5f4e46dfa2c25b163e8eabba136
Jeroen Smeets <jeroen@jeroensmeets.net>
2020-03-19 16:05:35 +0100

    add Azure application insights

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import router from "./router";
 
 Vue.config.productionTip = false;
 
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
+
+const appInsights = new ApplicationInsights({
+  config: {
+    instrumentationKey: '34c97fb7-5afa-4077-b608-47b0cfd02394'
+  }
+});
+appInsights.loadAppInsights();
+appInsights.trackPageView(); // Manually call trackPageView to establish the current user/session/pageview
+
 new Vue({
   router,
   render: h => h(App)

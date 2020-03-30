2697cb4e879c2275ea8dc82994d712b6be874516
Igor Afanasov <iafanasov@mobiquityinc.com>
2020-03-30 15:45:12 +0200

    Google map without default controls, with focus on the current user location
    All the vuejs google maps libraries I found are not maintained properly. Using google JS API directly
    
    Closes #80

--- src/views/LetMeHelp.vue
+++ src/views/LetMeHelp.vue
@@ -1,18 +1,65 @@
 <template>
-  <div class="questions">
-    <h1>Start!</h1>
+  <div>
+    <div ref="googleMap" class="google-map"></div>
   </div>
 </template>
 
 <script>
+import GoogleMapsApiLoader from 'google-maps-api-loader'
+
 export default {
   name: 'LetMeHelp',
+  components: {},
+  data: () => {
+    return {
+      google: null,
+      map: null,
+      mapConfig: {
+        // Amsterdam central station
+        center: { lat: 52.379189, lng: 4.899431 },
+        zoom: 13,
+        disableDefaultUI: true,
+      },
+    }
+  },
+  async mounted() {
+    const googleMapApi = await GoogleMapsApiLoader({
+      apiKey: process.env.VUE_APP_GOOGLE_MAPS_API_KEY,
+      language: 'nl',
+    })
+    this.google = googleMapApi
+    this.initializeMap()
+  },
+  methods: {
+    initializeMap() {
+      const mapContainer = this.$refs.googleMap
+      this.map = new this.google.maps.Map(mapContainer, this.mapConfig)
+      this.navigateMapToTheCurrentLocation()
+    },
+    navigateMapToTheCurrentLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          }
+
+          this.map.setCenter(pos)
+        })
+      }
+    },
+  },
 }
 </script>
 
 <style scoped lang="scss">
 @import '@design';
-.questions {
-  background-color: $color-blue-light;
+
+$size-distance-from-margin: 2rem;
+.google-map {
+  width: 100%;
+  // TODO review markup with https://github.com/I-OWE-YOU/web-frontend/issues/78
+  height: calc(100vh - #{$size-header-height + $size-distance-from-margin});
+  margin-top: $size-distance-from-margin;
 }
 </style>

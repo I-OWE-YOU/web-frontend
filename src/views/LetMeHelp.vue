3986e0ccc62e6982fa2a0dc8586467d0bf1940d0
Igor Afanasov <iafanasov@mobiquityinc.com>
2020-03-31 08:23:31 +0200

    Added field to navigate to the location
    Closes #66

--- src/views/LetMeHelp.vue
+++ src/views/LetMeHelp.vue
@@ -1,19 +1,32 @@
 <template>
   <div>
+    <form @submit.prevent="navigateToAddress">
+      <FormField
+        v-model="search"
+        field-name="search"
+        field-label="Zoek via postcode of plaatsnaam"
+        :required="true"
+      />
+    </form>
+
     <div ref="googleMap" class="google-map"></div>
   </div>
 </template>
 
 <script>
 import GoogleMapsApiLoader from 'google-maps-api-loader'
+import FormField from '@components/FormField.vue'
 
 export default {
   name: 'LetMeHelp',
-  components: {},
+  components: {
+    FormField,
+  },
   data: () => {
     return {
       google: null,
       map: null,
+      search: '',
       mapConfig: {
         // Amsterdam central station
         center: { lat: 52.379189, lng: 4.899431 },
@@ -28,6 +41,7 @@ export default {
       language: 'nl',
     })
     this.google = googleMapApi
+    this.geocoder = new window.google.maps.Geocoder()
     this.initializeMap()
   },
   methods: {
@@ -47,6 +61,16 @@ export default {
           this.map.setCenter(pos)
         })
       }
+    },
+    navigateToAddress() {
+      const request = { address: this.search, location: this.map.getCenter() }
+      this.geocoder.geocode(request, (results, status) => {
+        if (status === 'OK') {
+          this.map.setCenter(results[0].geometry.location)
+        } else {
+          alert('Address was not found for the following reason: ' + status)
+        }
+      })
     },
   },
 }

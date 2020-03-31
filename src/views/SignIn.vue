8dd45054fd5351063857721f4ecfd1e12a52936a
Petar Blazhevski <petar.blazevski@gmail.com>
2020-03-31 20:14:03 +0200

    fix typo

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -19,7 +19,7 @@
         :required="true"
       />
 
-      <button class="m-4" @click="signIn">Sign Ip</button>
+      <button class="m-4" @click="signIn">Sign In</button>
     </form>
   </div>
 </template>

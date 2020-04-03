2d9f15ba8886ec213739281e683c33e513510257
Petar Blazhevski <pblazhevski@mobiquityinc.com>
2020-04-03 08:47:00 +0200

    remove unused css

--- src/views/CouponConfirm.vue
+++ src/views/CouponConfirm.vue
@@ -141,32 +141,4 @@ export default {
 
 <style lang="scss">
 @import '@design';
-
-/**
-* The CSS shown here will not be introduced in the Quickstart guide, but shows
-* how you can use CSS to style your Element's container.
-*/
-.StripeElement {
-  box-sizing: border-box;
-  height: 40px;
-  padding: 10px 12px;
-  background-color: white;
-  border: 1px solid transparent;
-  border-radius: 4px;
-  box-shadow: 0 1px 3px 0 #e6ebf1;
-  -webkit-transition: box-shadow 150ms ease;
-  transition: box-shadow 150ms ease;
-}
-
-.StripeElement--focus {
-  box-shadow: 0 1px 3px 0 #cfd7df;
-}
-
-.StripeElement--invalid {
-  border-color: #fa755a;
-}
-
-.StripeElement--webkit-autofill {
-  background-color: #fefde5 !important;
-}
 </style>

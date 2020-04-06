e0244122f375f7657f24762915303865271eacc6
Petar Blazhevski <pblazhevski@mobiquityinc.com>
2020-04-06 11:45:16 +0200

    create a new step for connecting with Stripe

---
+++ src/components/entrepreneur-flow/EntrepreneurFlowStripeConnect.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h1>Direct en veilig je geld ontvangen</h1>
+
+    <p>
+      Om elke betaling veilig, transparant en snel te laten gebeuren, hebben we
+      gekozen voor het betrouwbare en onafhankelijke betaalplatform Stripe
+    </p>
+    <p
+      >Om verder te gaan, hoef je dus alleen een gratis account bij Stripe aan
+      te maken.</p
+    >
+
+    <button
+      class="big-red-button"
+      type="button"
+      :disabled="isLoading"
+      @click="getConnectUrl"
+      >Ga naar Stripe</button
+    >
+  </div>
+</template>
+
+<script>
+import { API } from 'aws-amplify'
+
+export default {
+  name: 'EntrepreneurFlowStripeConnect',
+  data() {
+    return {
+      isLoading: false,
+    }
+  },
+  methods: {
+    async getConnectUrl() {
+      this.isLoaded = true
+
+      try {
+        const res = await API.get('BackendAPIDev', '/stripe/connect', {})
+        window.location.href = res
+      } catch (e) {
+        this.isLoaded = false
+        console.error(e)
+      }
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+@import '@design';
+</style>
--- src/views/Entrepreneur.vue
+++ src/views/Entrepreneur.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="questions">
-    <EntrepreneurFlowPersonName v-if="step === steps.personName" :entrepreneur="entrepreneur"></EntrepreneurFlowPersonName>
+    <EntrepreneurFlowPersonName
+      v-if="step === steps.personName"
+      :entrepreneur="entrepreneur"
+    ></EntrepreneurFlowPersonName>
     <EntrepreneurFlowBusinessInfo
       v-else-if="step === steps.companyName"
       :entrepreneur="entrepreneur"
     ></EntrepreneurFlowBusinessInfo>
-    <EntrepreneurFlowSetupPayment v-else-if="step === steps.setupPayment"></EntrepreneurFlowSetupPayment>
-    <EntrepreneurFlowFinish v-else-if="step === steps.finish"></EntrepreneurFlowFinish>
+    <EntrepreneurFlowStripeConnect
+      v-else-if="step === steps.stripeConnect"
+    ></EntrepreneurFlowStripeConnect>
+    <EntrepreneurFlowFinish
+      v-else-if="step === steps.finish"
+    ></EntrepreneurFlowFinish>
 
     <div v-show="isWaitingForApiResponse" id="spinner" role="presentation">
       <img src="../assets/img/timer.svg" />
@@ -19,7 +26,7 @@ import axios from 'axios'
 import EntrepreneurFlowPersonName from '@components/entrepreneur-flow/EntrepreneurFlowPersonName.vue'
 import EntrepreneurFlowBusinessInfo from '@components/entrepreneur-flow/EntrepreneurFlowBusinessInfo.vue'
 import EntrepreneurFlowFinish from '@components/entrepreneur-flow/EntrepreneurFlowFinish.vue'
-import EntrepreneurFlowSetupPayment from '@components/entrepreneur-flow/EntrepreneurFlowSetupPayment.vue'
+import EntrepreneurFlowStripeConnect from '@components/entrepreneur-flow/EntrepreneurFlowStripeConnect.vue'
 import { EventBus } from '@plugins/event-bus.js'
 
 export default {
@@ -27,16 +34,16 @@ export default {
   components: {
     EntrepreneurFlowPersonName,
     EntrepreneurFlowBusinessInfo,
-    EntrepreneurFlowSetupPayment,
+    EntrepreneurFlowStripeConnect,
     EntrepreneurFlowFinish,
   },
   data: () => {
     return {
-      step: 1,
+      step: 2,
       steps: {
         personName: 0,
         companyName: 1,
-        setupPayment: 2,
+        stripeConnect: 2,
         finish: 3,
       },
       isWaitingForApiResponse: false,

82f84d4390881ee4b8174123dd9e48d94de9ddec
Cezary Skura <cskura@mobiquityinc.com>
2020-04-14 08:42:41 +0200

    Get company name from query string and display on confirm page

--- src/views/SuccessfullyPayment.vue
+++ src/views/SuccessfullyPayment.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
     <h2>Gelukt</h2>
-    <p>
-      wilt je ontzettend bedanken voor je hulp in deze moeilijke tijden.
-    </p>
     <p
-      >Zodra jouw betaling is bevestigd ontvang je direct een e-mail met jouw
-      Tegoedje.</p
+      >{{ companyName }} wilt je ontzettend bedanken voor je hulp in deze
+      moeilijke tijden.</p
     >
+    <p>
+      Zodra jouw betaling is bevestigd ontvang je direct een e-mail met jouw
+      Tegoedje.
+    </p>
     <p>Laten we hopen dat de zaken weer gauw gaan lopen.</p>
   </div>
 </template>
@@ -15,5 +16,13 @@
 <script>
 export default {
   name: 'SuccessfullyPayment',
+  data: () => {
+    return {
+      companyName: '',
+    }
+  },
+  async mounted() {
+    this.companyName = this.$route.query.company_name
+  },
 }
 </script>
